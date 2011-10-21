4601e071df2a665e6a27ac13028be5e2b7dc8f91
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-21 20:36:37 +0000

    Added CSH Help texts with some explanations and hints about meta descriptions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/serppreview/trunk@53258 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,9 +3,11 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 if (TYPO3_MODE=='BE')	{
 
+	t3lib_extMgm::addLLrefForTCAdescr('pages', 'EXT:serppreview/Resources/Private/Language/locallang_csh_pages.xml');
+
 	$TCA['pages']['columns'] += array(
 		'tx_serppreview_preview' => array(
-			'label' => 'SERP Preview',
+			'label' => 'LLL:EXT:serppreview/Resources/Private/Language/locallang_db.xml:pages.tx_serppreview_preview',
 			'exclude' => 0,
 			'config' => array (
 				'type' => 'user',
